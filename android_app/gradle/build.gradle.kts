efb2571a5240f81349c964e8e86d32145b1d9d68
thatlkat <64574888+thatlkat@users.noreply.github.com>
2026-04-15 13:20:40 -0700

    Create build.gradle.kts

---
+++ android_app/gradle/build.gradle.kts
@@ -0,0 +1,6 @@
+// cafeOwner/android_app/build.gradle.kts
+plugins {
+    id("com.android.application") version "8.5.0" apply false // Use the latest version
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false // Use the latest version
+    id("com.google.gms.google-services") version "4.4.2" apply false // Use the latest version
+}
